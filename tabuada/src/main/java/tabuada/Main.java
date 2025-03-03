e62c42d7d3ee58b9777222fd028e1cb1e93c7ed5
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-03 11:06:11 -0300

    PSegundo Commit - Programa funcionando

--- tabuada/src/main/java/tabuada/Main.java
+++ tabuada/src/main/java/tabuada/Main.java
@@ -1,7 +1,22 @@
 package tabuada;
-
+import java.util.Scanner;
 public class Main {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Cria a variável número e pede para o usuário escolher o número a ser feita a tabuada, declarando a resposta na variável
+        double numero;
+        Scanner pergunta = new Scanner(System.in);
+        System.out.println("Digite o número: ");
+        numero = pergunta.nextDouble();
+        // Fecha o scanner que foi usado para perguntar o número au usuário
+        pergunta.close();
+
+        // Realiza a tabuada e imprime na tela resultado por resultado
+        for (int i = 1; i <= 10; i++) {
+            double resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+        
     }
 }
